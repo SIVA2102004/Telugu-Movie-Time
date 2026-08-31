f0d5f8469f98b0c2b933dc7b4cd9bc079fee8524
sivacollegee <sivacollegee@gmail.com>
2026-08-31 15:57:49 +0530

    Fix DEFAULT_SCREENS reference in MovieConfigEditor

--- src/components/MovieConfigEditor.jsx
+++ src/components/MovieConfigEditor.jsx
@@ -2,6 +2,7 @@ import { useState, useEffect } from "react";
 import { db } from "../firebase";
 import { doc, setDoc } from "firebase/firestore";
 import { Save, QrCode, Smartphone, CreditCard, KeyRound, UserCheck, ShieldCheck, Copy, IndianRupee, Tag } from "lucide-react";
+import { DEFAULT_SCREENS } from "../hooks/useMovieConfig";
 import toast from "react-hot-toast";
 import "./MovieConfigEditor.css";
 
